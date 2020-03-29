8fa3be9b73be711abf0c805983b0716f8d912b02
ge97wuc <michal.hetmanczuk@tum.de>
2020-03-29 12:38:56 +0200

    image preview component added

---
+++ frontend/src/app/photo-preview/photo-preview.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-preview',
+  templateUrl: './photo-preview.component.html',
+  styleUrls: ['./photo-preview.component.css']
+})
+export class PhotoPreviewComponent {
+  public imagePath;
+  imgURL: any;
+  public message: string;
+
+  preview(files) {
+    if (files.length === 0) {
+      return;
+    }
+
+    const mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = 'Only images are supported.';
+      return;
+    }
+
+    const reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]);
+    reader.onload = (event) => {
+      this.imgURL = reader.result;
+    };
+  }
+}
