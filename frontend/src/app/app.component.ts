a3de233cc27389b6c863a545862f7446a5777883
Anemiq <grododzierzca@gmail.com>
2020-03-29 13:36:00 +0200

    added flask server

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NSFW-frontend';
+  private _jsonURL = 'assets/config.json';
+  private paths: any = [];
+  selectedFile: File;
+  constructor(private http: HttpClient) {
+    this.getJSON().subscribe(data => this.paths=data, error => console.log(error));
+  }
+    onFileChanged(event){
+    this.selectedFile = event.target.files[0]
+    }
+
+    onUpload(){
+      const uploadData = new FormData();
+      uploadData.append('myFile', this.selectedFile, this.selectedFile.name);
+      this.http.post(this.paths['server-http']+this.paths['blur-path']+"/"+this.selectedFile.name, uploadData, {
+        reportProgress: true,
+        observe: 'events',
+      }).subscribe(event => {console.log(event);})
+
+    }
+
+    public getJSON(): Observable<any> {
+      return this.http.get(this._jsonURL);
+    }
 }
+
