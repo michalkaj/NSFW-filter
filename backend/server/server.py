b468ec659c2a328bd1c4b8501639a2dc370123d1
Anemiq <grododzierzca@gmail.com>
2020-03-30 13:00:46 +0200

    fixed server.py

---
+++ backend/server/server.py
@@ -0,0 +1,31 @@
+from flask import FLASK
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def index():
+    return 'This is index page'
+
+
+@app.route('/blur/<filename>', methods=['POST'])
+def blurImage(filename):
+    if request.method == 'POST':
+        return blurFunc(filename)
+    else:
+        return showPage()
+
+
+@app.route('/nsfw/<filename>', methods=['POST'])
+def nsfwFilter(filename):
+    return f'Filename {filename}'
+
+
+def blurFunc(filename):
+    return f'Blurring {filename}'
+
+
+def showPage():
+    return 'Upland here'
