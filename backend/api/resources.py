dfaa93bfa1763d7da341c788a52442985e5f2444
MMHetman <57961266+mmhetman@users.noreply.github.com>
2020-04-20 17:35:25 +0200

    Colors frontend

--- backend/api/resources.py
+++ backend/api/resources.py
@@ -16,7 +16,7 @@ class BlurImageResource(Resource):
     def __init__(self):
         super().__init__()
         self._face_blur_pipeline = self._init_pipeline()
-        self._request_parser  = reqparse.RequestParser()
+        self._request_parser = reqparse.RequestParser()
         self._request_parser.add_argument(IMAGE_FILE_NAME, type=FileStorage, location='files')
 
     def post(self):
@@ -46,4 +46,33 @@ class BlurImageResource(Resource):
 
 
 class BlurNSFW(Resource):
-    ...
+    def __init__(self):
+        super().__init__()
+        self._nudity_blur_pipeline = self._init_pipeline()
+        self._request_parser = reqparse.RequestParser()
+        self._request_parser.add_argument(IMAGE_FILE_NAME, type=FileStorage, location='files')
+
+    def post(self):
+        args = self._request_parser.parse_args()
+        image = Image.open(args[IMAGE_FILE_NAME])
+        blurred_image = self._face_blur_pipeline.apply(image)
+        image_bytes = self._image_to_bytes(blurred_image)
+
+        return send_file(image_bytes,
+                         mimetype='image/jpeg',
+                         as_attachment=True,
+                         attachment_filename='blur.jpg')
+
+    def _init_pipeline(self):
+        pipeline = Pipeline()
+        pipeline.add_step(Ensure3Channels())
+        pipeline.add_step(EnsureImageSize(MAX_IMAGE_SIZE, MAX_IMAGE_SIZE))
+        pipeline.add_step(BlurFaces())
+        return pipeline
+
+    @staticmethod
+    def _image_to_bytes(image):
+        image_bytes = io.BytesIO()
+        image.save(image_bytes, format='JPEG')
+        image_bytes.seek(0)
+        return image_bytes
--- backend/processors/images.py
+++ backend/processors/images.py
@@ -1,7 +1,12 @@
+import uuid
+from os import remove
+
 from PIL.Image import Image, fromarray
 
 from blur_face.blurring import GaussianBlur, ImageBlur
 from blur_face.detection import Detector
+from nudenet import NudeDetector
+from blur_face.bounding_box import BoundingBox
 
 
 class Ensure3Channels:
@@ -31,4 +36,21 @@ class BlurFaces:
        blurred_image_np = self._blur.blur(image, detected_faces)
        return fromarray(blurred_image_np)
 
+class CensorNudity:
+    def __init__(self, blur_mask_fade=2, kernel_size=51, sigma=10):
+        self._detector = NudeDetector()
+        self._labels_of_acceptable = ['BELLY', 'M_BREAST']
+        blur_method = GaussianBlur(kernel_size, sigma)
+        self._blur = ImageBlur(blur_method, blur_mask_fade)
+
+    def __call__(self, image: Image) -> Image:
+        path = str(uuid.uuid4())
+        Image.save(image, path)
+        detected_nudity = self._detector.detect(path)
+        remove(path)
+        nudity_boxes = [BoundingBox(*nudity['box'])
+                        for nudity in detected_nudity
+                        if nudity['label'] not in self._labels_of_acceptable]
+        blurred_image_np = self._blur.blur(image, nudity_boxes)
+        return fromarray(blurred_image_np)
 
